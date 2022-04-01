b8d95eec87bedd1253f613ffcfb9d1767484617f
Ann <kononovich.anna@gmail.com>
2022-04-01 07:28:04 -0700

    email module v4

--- mail/layouts/html.php
+++ mail/layouts/html.php
@@ -1,22 +1,55 @@
 <?php
-use yii\helpers\Html;
+
+use yii\helpers\Url;
 
 /** @var \yii\web\View $this view component instance */
 /** @var \yii\mail\MessageInterface $message the message being composed */
 /** @var string $content main view render result */
 ?>
 <?php $this->beginPage() ?>
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
-        <title><?= Html::encode($this->title) ?></title>
-        <?php $this->head() ?>
-    </head>
-    <body>
-    <?php $this->beginBody() ?>
-    <?= $content ?>
-    <?php $this->endBody() ?>
-    </body>
-    </html>
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>Need Call Back!</title>
+    <meta name="description" content="Reset Password Email Template.">
+    <style>
+        a:hover {
+            text-decoration: underline !important;
+        }
+    </style>
+</head>
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+    <tr>
+        <td>
+            <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        <?= $content ?>
+                    </td>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;">
+                        <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong><?= Url::base(true) ?></strong></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
 <?php $this->endPage() ?>
+
+
+
